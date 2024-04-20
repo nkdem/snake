0ce47977deb2442795167da1e84c3c4077ca2a8a
nkdem <dev@nkdem.net>
2024-04-20 14:27:12 +0100

    Complete randomisation of food spawning

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 A snake game made with C and raylib.
 
 ## TODOs
-- [] Render a window 
-- [] Randomly spawn a red square
+- [x] Render a window 
+- [x] Randomly spawn a red square
 - [] Implement snake movement
 - [] Implement snake collision
 - [] Add some music
--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "raylib.h"
 
+// STATIC
+int BORDER_SIZE = 10;
+
 int main(void) {
   const int screenWidth = 800;
   const int screenHeight = 450;
@@ -7,10 +10,28 @@ int main(void) {
   InitWindow(screenWidth, screenHeight, "window");
   SetTargetFPS(60);
 
+  bool eatten = false;
+  Vector2 food = {GetRandomValue(BORDER_SIZE, screenWidth - BORDER_SIZE),
+                  GetRandomValue(BORDER_SIZE, screenHeight - BORDER_SIZE)};
+
   while (!WindowShouldClose()) {
+    if (eatten) {
+      food = (Vector2){GetRandomValue(BORDER_SIZE, screenWidth - BORDER_SIZE),
+                       GetRandomValue(BORDER_SIZE, screenHeight - BORDER_SIZE)};
+      eatten = false;
+    }
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("hello world", 190, 200, 20, LIGHTGRAY);
+
+    // Draw a gray border
+    DrawLineEx((Vector2){0, 0}, (Vector2){screenWidth, 0}, BORDER_SIZE, GRAY);
+    DrawLineEx((Vector2){0, 0}, (Vector2){0, screenHeight}, BORDER_SIZE, GRAY);
+    DrawLineEx((Vector2){screenWidth, 0}, (Vector2){screenWidth, screenHeight},
+               BORDER_SIZE, GRAY);
+    DrawLineEx((Vector2){0, screenHeight}, (Vector2){screenWidth, screenHeight},
+               BORDER_SIZE, GRAY);
+
+    DrawRectangleRec((Rectangle){food.x, food.y, 10, 10}, RED);
     EndDrawing();
   }
 
